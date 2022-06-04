0b620ae9379709d1ab0db56aaff59b16ab2ceff0
TRACIE-F <99625130+TRACIE-F@users.noreply.github.com>
2022-06-04 09:16:15 -0500

    file updates and name swaps

--- static/js/app.js
+++ static/js/app.js
@@ -24,7 +24,9 @@ function buildTable(data) {
 }
 
 // 1. Create a variable to keep track of all the filters as an object.
-
+UFOsearch = [
+  {}
+]
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
